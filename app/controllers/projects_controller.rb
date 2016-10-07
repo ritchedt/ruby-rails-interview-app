0cae46bd20021045927ec1946cdb4bc15d235d70
ritchedt <ritchedt@miamioh.edu>
2016-10-07 14:22:12 -0400

    Added viewing of Projects and descriptions to be separated into 'show' pages
    Reason: Instead of the default ul view of seeing projects and
    descriptiosn, I added in some show code with links to separate the pages
    
    Files changed:
    projects_controller.rb
    project.rb
    projects\show.html.erb
    _project.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.paginate(page: params[:page])
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
 end
