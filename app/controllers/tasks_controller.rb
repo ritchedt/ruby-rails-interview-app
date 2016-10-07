6edc0d5bb41fa47a63ac379643611e97131f868d
ritchedt <ritchedt@miamioh.edu>
2016-10-07 11:36:28 -0400

    Removed unneeded code for Tasks
    No need for the show method, removed code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,6 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
-
   def import
     Task.import(params[:file])
     redirect_to tasks_path, notice: "Employee Project Data Imported!"
