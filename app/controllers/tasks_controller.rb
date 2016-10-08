b52f8b08dd9896fcb0b0ade40cb7ec9c732f22ce
ritchedt <ritchedt@miamioh.edu>
2016-10-08 18:04:10 -0400

    Link to see employee contributions
    Reason: Added links to see each project that an employee is a part of
    
    Files changed:
    tasks_controller.rb
    tasks\index.html.erb
    tasks\show.html.erb
    _task.html.erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+    @project = Project.all
+  end
+
   def import
     Task.import(params[:file])
     redirect_to tasks_path, notice: "Employee Project Data Imported!"
