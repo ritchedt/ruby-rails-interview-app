1eff58ed3b6ecc92eba259b14b90fab192c02256
ritchedt <ritchedt@miamioh.edu>
2016-10-08 13:08:28 -0400

    Coorelate Materials with Project
    Reason: Provided coorelation to materials and project to get more
    information about each project description
    
    Files changed:
    projects_controller.rb
    materials\index.html.erb
    _materials.html.erb
    projects\show.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @material = Material.where(:project => Project.find(params[:id]).name)
   end
 
 end
