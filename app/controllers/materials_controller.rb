1ea61d57c36794ae471d746e334a7880fefade51
ritchedt <ritchedt@miamioh.edu>
2016-10-09 11:46:15 -0400

    Updated Materials Inventory page
    Reason: All materials page is now updated to display all materials in
    their updated amount
    
    Files changed:
    materials_controller.rb
    materials\index.html.erb
    _materials.html.erb

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,11 +1,23 @@
 class MaterialsController < ApplicationController
 
   def index
-    @materials = Material.all
+    @materials = Material.select("distinct material", "amount")
   end
 
   def import
     Material.import(params[:file])
     redirect_to materials_path, notice: "Project Material Data Imported!"
   end
+
+  def get_total_material_amount(material, amount)
+    if material.casecmp("Brick") != 0 && material.casecmp("sand") != 0 &&
+        material.casecmp("cement") != 0 && material.casecmp("carpet") != 0 && material.casecmp("glass") != 0
+      total = amount[/(\d*)\D*/, 1]
+    else
+      total = amount
+    end
+    total.to_i
+  end
+
+  helper_method :get_total_material_amount
 end
